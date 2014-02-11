9d17811a9592860b89270d7a6955323decb4072e
William <wpkp34@gmail.com>
2014-02-11 10:28:35 +0000

    !mapnr and other improvements
    - Admin messages for !respawn and !givemagnum
    - Added !mapnr
    - Improved hooks
    - Updated README

--- README.md
+++ README.md
@@ -2,9 +2,10 @@ A collection of my addons/scripts
 ==========
 Feel free to download or view my addons. I'd appreciate any recommendations for improvements.
 
-ULX_GrabScreen: Adds an in-game command allowing users to receive a low-quality screenshot from the target, showing exactly what they see inside their window at any given moment. Useful for catching hackers in the act. You can also save the image locally, to then upload as evidence requesting a ban.
+ULX_GrabScreen: Adds an in-game command allowing users to receive a low-quality screenshot from the target, showing exactly what they see inside their GMod window (including the steam overlay) at any given moment. Useful for catching hackers in the act. You can also save the image locally, to then upload as evidence for ban requests.
 
 ULX_Murder: A set of ULX commands for usage specifically with the Murder gamemode
-- !slaynr - Slay the target at the beggining of the next round. Automatically bans them if they leave with active autoslays. Prevents them from spawning as the Murderer to stop rounds being cut short. Additional configuration in file.
+- !slaynr - Slay the target at the beggining of the round for as many rounds as you specify them. Automatically bans them if they leave with active autoslays. Prevents them from spawning as the Murderer to stop rounds being cut short. Additional configuration in file. You can remove all active slays if the player is on the server using !unslaynr
 - !givemagnum - Give the target a magnum revolver (cannot give to the Murderer)
-- !respawn - Respawn the target (TODO: Prevent spectators from being respawned to prevent naming issues + chat notification)
+- !respawn - Respawns the target
+- !mapnr - Changes the map at the beginning of the next round, including notifications when it is set and at the end of the round. Can be removed at any time using !unmapnr.
--- addons/ULX_Murder/lua/ulx/modules/sh/murder.lua
+++ addons/ULX_Murder/lua/ulx/modules/sh/murder.lua
@@ -1,19 +1,6 @@
-CATEGORY_NAME = "Murder"
-
-//////////////////////////////////////////////////////////
-//												    	//
-//				SLAY NEXT ROUND (!slaynr)				//		   
-//	This command slays the target at the beggining of 	//
-//	the next round, preventing them from spawning as	//
-//	the Murderer to stop rounds being cut short. You	//
-//	can also specify the amount of rounds the player 	//
-//	should be slain for. Slays do not count if targets 	//
-//	are in spectator mode, and carry on after the 		//
-//	target disconnects (if enabled).					//
-//														//
-//////////////////////////////////////////////////////////
-
 --[[			CONFIG 			]]--
+CATEGORY_NAME = "Murder"
+// The name that the commands will be placed under
 local slaynr_ban = true
 // Should players with autoslays be banned if they disconnect?
 // (true/false)
@@ -27,11 +14,21 @@ local slaynr_keepslays = false
 // Should the player keep the slays they have if they get banned for slay evading?
 // (true/false)
 
-function ulx.slaynr( caller, targets, rounds, shouldslay )
+--------------- Don't edit anything below this line -------------------
+---------------- Unless you know what you're doing --------------------
+
+if SERVER then
+	hook.Remove( "OnStartRound", "RoundStartEvents" )
+	hook.Remove( "PlayerInitialSpawn", "PreventReconnectingMurderer" )
+	hook.Remove( "PlayerDisconnected", "BanThemSlayEvaders" )
+	hook.Remove( "OnEndRound", "NotifyMapChange" )
+end
+
+function ulx.slaynr( caller, targets, rounds, unset )
 
 	for k, v in pairs( targets ) do
 		
-		if not shouldslay then
+		if not unset then
 			v:SetPData( "NRSLAY_SLAYS", rounds )
 			v.MurdererChance = 0
 			str = "#A set #T to be autoslain for "..rounds.." round(s)"
@@ -54,9 +51,93 @@ slaynr:defaultAccess( ULib.ACCESS_ADMIN )
 slaynr:help( "Slays the target at the beggining of the next round." )
 slaynr:setOpposite( "ulx unslaynr", { _, _, _, true }, "!unslaynr" )
 
+function ulx.givemagnum( caller, targets )
+
+	for k, v in pairs( targets ) do
+		
+		if v:GetObserverTarget() then
+			ULib.tsayError( caller, "Spectators cannot be given a magnum!", true ) continue
+		elseif v:GetMurderer() then
+			ULib.tsayError( caller, "The Murderer cannot be given a magnum!", true ) continue
+		end
+
+		v:Give( "weapon_mu_magnum" )
+		ulx.fancyLogAdmin( caller, false, "#A gave #T a magnum", targets )
+
+	end
+
+end
+local magnum = ulx.command( CATEGORY_NAME, "ulx givemagnum", ulx.givemagnum, "!givemagnum" )
+magnum:addParam{ type=ULib.cmds.PlayersArg, ULib.cmds.optional }
+magnum:defaultAccess( ULib.ACCESS_ADMIN )
+magnum:help( "Give the target the magnum." )
+
+function ulx.respawn( caller, targets )
+
+	for k, v in pairs( targets ) do
+
+		if v:Team() ~= 2 then ULib.tsayError( caller, "You cannot respawn a spectator!" ) return end
+
+		v:Spectate( OBS_MODE_NONE )
+		v:Spawn()
+
+	end
+
+	ulx.fancyLogAdmin( caller, false, "#A respawned #T", targets )
+
+end
+respawn = ulx.command( CATEGORY_NAME, "ulx respawn", ulx.respawn, "!respawn" )
+respawn:addParam{ type=ULib.cmds.PlayersArg }
+respawn:defaultAccess( ULib.ACCESS_ADMIN )
+respawn:help( "Respawn the target." )
+
+if SERVER then
+	mapnr_changemap = false
+	mapnr_map = nil
+end
+function ulx.mapnr( caller, map, unset )
+
+	if !table.HasValue( ulx.maps, map ) and not unset then
+		ULib.tsayError( caller, "Invalid map \""..map.."\" specified!" )
+		return
+	end
+
+	if !mapnr_changemap and unset then
+		ULib.tsayError( caller, "There is no map change active!" )
+		return
+	end
+
+	if not unset then
+		str = "#A set the map to be changed to #s next round!"
+		mapnr_changemap = true
+		mapnr_map = map
+	else
+		str = "#A removed the map change to #s next round!"
+		mapnr_changemap = false
+	end
+
+	ulx.fancyLogAdmin( caller, false, str, mapnr_map )
+
+end
+mapnr = ulx.command( CATEGORY_NAME, "ulx mapnr", ulx.mapnr, "!mapnr" )
+mapnr:addParam{ type=ULib.cmds.StringArg, completes=ulx.maps, hint="map", ULib.cmds.optional }
+mapnr:addParam{ type=ULib.cmds.BoolArg, invisible=true }
+mapnr:defaultAccess( ULib.ACCESS_ADMIN )
+mapnr:help( "Changes the map at the beggining of the next round." )
+mapnr:setOpposite( "ulx unmapnr", { _, _, true }, "!unmapnr" )
+
+
+/////			Hooks 			/////
+
+
 if SERVER then
 
-	hook.Add( "OnStartRound", "Autoslays", function()
+	hook.Add( "OnStartRound", "RoundStartEvents", function()
+
+		if mapnr_changemap then
+			mapnr_changemap = false
+			game.ConsoleCommand( "changelevel "..mapnr_map.."\n" )
+		end
 
 		timer.Simple( 3, function()
 			
@@ -80,7 +161,7 @@ if SERVER then
 					ulx.fancyLogAdmin( nil, false, "Autoslayed #T", v )
 					v:Kill()
 					if reconnect then
-						ULib.tsay( v, "You have been autoslain after leaving with active autoslays", true )
+						ULib.tsay( v, "You have been autoslain after leaving with active autoslays" )
 						v:RemovePData( "NRSLAY_LEAVE")
 					end
 
@@ -89,6 +170,14 @@ if SERVER then
 			end
 
 		end )
+
+	end )
+
+	hook.Add( "OnEndRound", "NotifyMapChange", function()
+		
+		if mapnr_changemap then
+			ulx.fancyLogAdmin( nil, false, "The map will change to #s next round!", mapnr_map )
+		end
 
 	end )
 
@@ -110,7 +199,7 @@ if SERVER then
 			if slays > 0 then
 				local reason, mins = "Attempting to evade "..slays.." autoslays.", ( slaynr_culumativeban and slays*slaynr_banmins or slaynr_banmins )
 				ULib.kickban( ply, mins, reason )
-				ulx.fancyLogAdmin( "The Server", "#A automatically banned #T for #i minutes (#s)", ply, mins, reason )
+				ulx.fancyLogAdmin( nil, false, "Automatically banned #T for #i minutes (#s) ", ply, mins, reason )
 				if !slaynr_keepslays then
 					ply:RemovePData( "NRSLAY_SLAYS" )
 				else
@@ -125,39 +214,3 @@ if SERVER then
 	end )
 
 end
-
-function ulx.givemagnum( caller, targets )
-
-	for k, v in pairs( targets ) do
-		
-		if v:GetObserverTarget() then
-			ULib.tsayError( caller, "Spectators cannot be given a magnum!", true ) continue
-		elseif v:GetMurderer() then
-			ULib.tsayError( caller, "The Murderer cannot be given a magnum!", true ) continue
-		end
-
-		v:Give( "weapon_mu_magnum" )
-
-	end
-
-end
-local magnum = ulx.command( CATEGORY_NAME, "ulx givemagnum", ulx.givemagnum, "!givemagnum" )
-magnum:addParam{ type=ULib.cmds.PlayersArg, ULib.cmds.optional }
-magnum:defaultAccess( ULib.ACCESS_ADMIN )
-magnum:help( "Give the target the magnum." )
-
-function ulx.respawn( caller, targets )
-
-	for k, v in pairs( targets ) do
-		if !v:IsValid() then return end
-		
-		v:Spawn()
-
-	end
-
-
-end
-respawn = ulx.command( CATEGORY_NAME, "ulx respawn", ulx.respawn, "!respawn" )
-respawn:addParam{ type=ULib.cmds.PlayersArg, ULib.cmds.optional }
-respawn:defaultAccess( ULib.ACCESS_ADMIN )
-respawn:help( "Respawn the target." )
